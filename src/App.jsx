689a33b204c889d7b322fe954fc021487bf7e832
Justyna <justynka.takk@gmail.com>
2021-09-19 20:21:11 +0200

    fetching data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
 function App() {
-  return <div className="App"></div>;
+  const [query, setQuery] = useState('');
+  const [word, setWord] = useState({});
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${query}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery('');
+          setWord(result);
+          console.log(result);
+        });
+    }
+  };
+
+  return (
+    <div className="App">
+      <section>
+        <input
+          type="text"
+          placeholder="SEARCH"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        ></input>
+      </section>
+      <section></section>
+    </div>
+  );
 }
 
 export default App;
