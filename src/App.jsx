f3253985669c61894b66bb725e8155dd228c78a8
Justyna <justynka.takk@gmail.com>
2021-09-25 17:58:12 +0200

    spinner moved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Synonim from './Synonim';
+import Synonim from './components/Synonim';
 import Spinner from './components/Spinner';
 
 function App() {
